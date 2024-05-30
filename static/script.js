c516fd8db48b715692944a48bc3e505cca914ba4
Igor Westphal <westphaligor2@gmail.com>
2024-05-30 14:40:44 +0200

    Verändet Funktionalität mit Video-HTML-Layout

--- static/script.js
+++ static/script.js
@@ -10,16 +10,6 @@ function toggleMenu() {
         menuButton.style.padding = '10px 20px';
     }
 }
-  let table_name = document.getElementsByTagName('a')[0].id  ;
-  console.log(table_name);
- $('#' + table_name).on('click', function (e) {
-    e.preventDefault();
-    this.obj = {};
-    this.obj.table_name = table_name
-    const data = JSON.stringify(this.obj);
-    console.log(data)
-        
- });
     
     
   
