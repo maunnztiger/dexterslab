b3d093cb4f1479d6f83f68d1dbb91292351eb662
Igor Westphal <westphaligor2@gmail.com>
2024-10-18 13:37:07 +0200

    work in progress

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 sudo rm -rf venv
+sleep 1
 sudo python3 -m virtualenv venv
+sleep 1
 sudo chown -R igor:igor venv/ && sudo chmod -R 777 venv/
+sleep 1
 source venv/bin/activate
+sleep 1
 pip install -r requirements.txt
+sleep 1
 python3 main.py
